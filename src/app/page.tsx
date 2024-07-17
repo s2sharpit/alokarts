dba41445186470d7034e5be0d4bd9c420bfcc17b
Tushar Saini <s2sharpit@gmail.com>
2024-07-17 14:55:52 +0530

    feat: add image slider component for improved user experience
    Signed-off-by: Tushar Saini <s2sharpit@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,26 +2,12 @@ import Products from "@/components/Products";
 import PopularProducts from "@/components/PopularProducts";
 import { Section } from "@/components/ui";
 import Image from "next/image";
+import ImgSlider from "@/components/ImgSlider";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-8 md:p-20">
-      <Section className="w-full grid sm:grid-cols-2 gap-8 md:gap-16 pt-4">
-        <Image
-          src="/1.jpg"
-          width={500}
-          height={300}
-          alt="alok main"
-          className="rounded-2xl w-full"
-        />
-        <Image
-          src="/2.jpg"
-          width={500}
-          height={300}
-          alt="alok main"
-          className="rounded-2xl w-full"
-        />
-      </Section>
+      <ImgSlider />
 
       <PopularProducts />
       <Products />
---
+++ src/components/ImgSlider.tsx
@@ -0,0 +1,47 @@
+"use client";
+import { Section } from "./ui";
+import productData from "@/data/productData";
+import { Navigation, Pagination, Autoplay, A11y } from "swiper/modules";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import Image from "next/image";
+
+export default function ImgSlider() {
+  return (
+    <Section>
+      <Swiper
+        className="h-full rounded-2xl overflow-hidden  md:w-4/5 border border-gray-400"
+        loop={true}
+        modules={[Autoplay, Navigation, Pagination, A11y]}
+        spaceBetween={24}
+        slidesPerView={1}
+        slidesPerGroup={1}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        navigation
+        pagination={{ clickable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log("slide change")}
+      >
+        {[1, 2, 3].map((data: any, index: number) => (
+          <SwiperSlide key={index}>
+            <div className="flex flex-col items-center rounded-2xl overflow-hidden">
+              <div className="h-48w-44 grid place-items-center">
+                <Image
+                  src={`/${index < 3 ? index + 1 : index % 3}.jpg`}
+                  alt=""
+                  width={1200}
+                  height={1000}
+                  className=""
+                />
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </Section>
+  );
+}
